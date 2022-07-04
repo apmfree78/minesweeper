2591190311cc88c6332b2b05831f287b10ccc389
apmfree78 <amit@profitswami.com>
2022-07-04 12:55:46 -0400

    bug fix - hard difficulty was not working

--- src/library/gameSpecs.tsx
+++ src/library/gameSpecs.tsx
@@ -3,4 +3,4 @@
 // various sizes , width and height, user can choose
 export const gameSizeOptions: number[] = [10, 15, 20];
 
-export const gameLevels: string[] = ['easy', 'medium', 'difficult'];
+export const gameLevels: string[] = ['easy', 'medium', 'hard'];
